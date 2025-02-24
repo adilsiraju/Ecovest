7a2562d53e5933cb693a41ae19b9c2ba0f1e0b83
Mohammed Adil Siraju <mohdadilsiraju@gmail.com>
2025-02-24 13:04:46 +0530

    1.12

--- ecovest/settings.py
+++ ecovest/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'django.contrib.humanize',
     'initiatives.apps.InitiativesConfig',
     'investments.apps.InvestmentsConfig',
     'bootstrap5',
