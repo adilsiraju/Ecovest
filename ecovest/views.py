7958fa8a25cf2cbb7f70745af5206ab801f05bd9
Mohammed Adil Siraju <mohdadilsiraju@gmail.com>
2025-03-02 14:17:08 +0530

    Style 1.0

--- ecovest/views.py
+++ ecovest/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth import logout
 
 def landing_page(request):
+    if request.user.is_authenticated:
+        logout(request)
     return render(request, 'landing/fullscreen.html')
